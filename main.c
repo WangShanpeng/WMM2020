cccc04731bcff2dfeeb2c0373d860d53be59e13f
WangShanpeng <wangshanpeng169@163.com>
2021-09-09 17:26:23 +0800

    添加项目文件。

---
+++ main.c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#include "wmm2020.h"
+
+int main()
+{
+	double decl, incl;
+	GetDecInc(&decl, &incl, 39, 116, 2021, 1);
+	printf("Decl:%f Incl:%f \n\r", decl * 180 / M_PI, incl * 180 / M_PI);
+
+	return 0;
+}
